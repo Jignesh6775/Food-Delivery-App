4878c2534500c65384ef053b35d0e88f92e6dfbd
Jignesh <jigsvadiyatar6557@gmail.com>
2023-07-10 22:39:07 +0530

    Get ROute

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require("dotenv").config()
-const port = process.env.PORT || 8080
+const port = process.env.PORT
 const { connection } = require("./Connections/db")
 const { restaurantRouter } = require("./Routes/restaurant.routes")
 const { userRouter } = require("./Routes/user.routes")
